ae94407dee536a9387457c0730c962baf50140f6
michaelthearnold <michaelthearnold@gmail.com>
2015-01-08 18:26:34 -0800

    Started phone gap integration

---
+++ www/Client/JS/Main.js
@@ -0,0 +1,37 @@
+/* Entry point for the client */
+var isPhonegapApp = document.URL.indexOf("http://") === -1 && document.URL.indexOf( 'https://' ) === -1
+
+if(isPhonegapApp)
+  document.addEventListener("deviceready", _startApp, true)
+
+else
+  _startApp()
+
+function _startApp()
+  {
+  //configure requirejs
+  requirejs.config(
+  {
+    //set base folder to root of server
+    baseUrl: "./../../",
+    paths: 
+    {
+      underscore: "Shared/3rdParty/underscore-min",
+      jquery: "Shared/3rdParty/jquery"
+    }
+  });
+
+  //boot up requirejs
+
+  requirejs(["Shared/3rdParty/phonegapApp", "Client/JS/Application"], function(phonegapApp, Application)
+  {
+  try{
+    //boot up webapp
+    var app = Application()
+    }  
+  catch(err)
+  {
+    document.body.innerHTML = err
+  }
+  })
+}
---
+++ www/Client/JS/Register.js
@@ -0,0 +1,89 @@
+//Client side Cart page.
+//Handles all of cart's calls to the server,
+//and creation of the cart page
+
+//define module for requirejs
+define(["Client/JS/Page"], 
+function(Page)
+{
+  
+  function Register()
+  {
+    /* PUBLIC METHODS */
+    
+    
+     
+    /* PRIVATE METHODS */
+    
+    function _register(event)
+    {
+      var ractive = this
+      
+      //collect required data for registration
+      var email = ractive.get("User.Email")
+      var name = ractive.get("User.Name")
+      var password = ractive.get("User.Password")
+      var phone = ractive.get("User.Phone")
+      
+      //Time to register the account.
+      //disable any further attempts to register
+      ractive.set("Processing", true)
+      
+      //send ajax request to server
+      var posting = $.post("RegisterUser", {pkg: JSON.stringify({email: email, password: password, name: name, phone: phone})})
+      posting.done(function(response)
+      {
+        ractive.set("Processing", false)
+        //if a response was given, parse it for its value
+        if(response)
+          response = JSON.parse(response.pkg)
+        //if the parsed response has content
+        ractive.set("Err", response.err)
+      })
+    }
+    
+    //Required to inherit from class Page
+    //This method initiates ractive
+    //binding data and logic to the front end HTML
+    function _attachRactive()
+    {      
+      //bind page to container
+      var component = Ractive.extend({
+        template: Templates["Register.html"],
+        data:
+        { 
+          User: {},
+          Processing: false,
+          Err: false
+        },
+        init: function()
+        {
+          ractive = this
+          
+          this.on("RegisterClick", _register)
+        }
+      })
+      
+      Ractive.components.Register = component
+    }
+    
+    
+    
+    
+    
+    /* CONSTRUCTOR */ 
+   
+    
+    //local variables
+    var ractive
+    _attachRactive()
+    
+    return{
+      __proto__: Page()
+    }
+  }
+  
+  return Register
+})
+
+
